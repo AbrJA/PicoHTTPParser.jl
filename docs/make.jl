2be38a9636a5601ea3aa9cd02d1416a22b0a82ea
AbrJA <abraham.jaimes.mx@gmail.com>
2025-10-21 18:38:42 -0600

    Bug fix for documentation and testing

--- docs/make.jl
+++ docs/make.jl
@@ -2,11 +2,12 @@ using Documenter, PicoHTTPParser
 
 makedocs(modules = [PicoHTTPParser],
          sitename = "PicoHTTPParser.jl",
+         authors = "Abraham Jaimes",
          format = Documenter.HTML()
          )
 
 deploydocs(
-    repo = "github.com/AbrJA/PicoHTTPParser.jl.git",
+    repo = "github.com/AbrJA/PicoHTTPParser.jl",
     target = "build",
     deps   = nothing,
     make   = nothing,
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,7 +1,10 @@
 # PicoHTTPParser
 
-PicoHTTPParser Julia package repo.
+Julia wrapper of high performance picohttpparser C library.
 
 ```@autodocs
 Modules = [PicoHTTPParser]
 ```
+
+## Contributing
+If you find any bug or improvement make the PR, contributions are welcome!
